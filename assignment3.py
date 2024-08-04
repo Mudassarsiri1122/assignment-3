5dde9e282e2c02dd15596acf2eee933834efff44
Hashir Ali Hassan <hashiralihassan51@gmail.com>
2024-08-04 10:31:06 -0700

    assignment 3

---
+++ assignment3.py
@@ -0,0 +1,86 @@
+#Questionn 1
+message:str="mairi assignment bht achi hai poora number do sir jahnzaib"
+print(message.title())
+#Questionn 2
+message:str="hy ap kaisa ho"
+print(message)
+message:str="g allhumdullilah"
+print(message)
+#Questionn 3
+x:str="Hello Mudassar"
+print(f"{x} would you like to learn some python today?".title())
+#Questionn 4
+x:str="Mudassar Hassan"
+print(x.upper())
+print(x.lower())
+print(x.title())
+#Questionn 5
+quote:str="Expect the best, prepare for the worst"
+name:str="Quaid e Azam"
+print(f"{name}once said {quote}".title())
+#Question 6
+famous_person:str="Quaid e Azam"
+message=f"{famous_person} Expect the best, prepare for the worst"
+print(message)
+#Question 7
+name = " Mudassar Hassan "
+print("Original name for white space")
+print(f"{name}")
+print(              "\nName after lstrip():")
+print(f"{name}")
+print("\nName after rstrip():             ")
+print(f"{name}".rstrip())
+print("\n Name after strip():")
+print(f"{name}".strip())
+#Question 8
+x = 5
+y = 10
+z = 15
+sum = x+y+z
+print("the sum of x,y and z is :",{sum})
+#Question 9
+a = 10
+b = 20
+print("Before swap:")
+print("a =",a)
+print("b =",b)
+a,b = b,a
+print("After swap:")
+print("a =",a)
+print("b =",b)
+#Question 10
+favouritecolour = "Yellow"
+print(f"favourite colour:{favouritecolour}")
+newcolour = favouritecolour
+print(f"New colour: {newcolour}")
+#Question 11
+petname = "cat"
+petname = "dog"
+print(f"the new pet name:{petname}")
+# #Question 12
+# name="sunshine"
+# print(f"the variable name contain:{name}")
+# print(f"the variable 'sunshine':{sunshine}")
+#Question 13
+score:int=100
+print(f"initial score:",score)
+score:int=120
+print(f"New score {score}")
+#Question 14
+city:str="Lahore"
+print(f"the city name is :{city}")
+#Question 15
+text:str="python program"
+print(f"Text in title case:{text}")
+#Question 16
+mixedcase:str="PYTHON PROGRAM"
+print(f"Text in lower case:{mixedcase.lower()}")
+#Question 17
+mixedcase:str="PYTHON PROGRAM"
+print(f"Text in upper case:{mixedcase.upper()}")
+#Question 18
+temperature:int=25
+print(f"the current temperature is:{temperature}degree")
+#Question 19
+poem =f'''Poetry is not only dream and vision; it is the skeleton architecture of our lives. It lays the foundations for a future of change, a bridge across our fears of what has never been before.'''
+print(poem)
